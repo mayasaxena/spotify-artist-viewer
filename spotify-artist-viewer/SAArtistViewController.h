c3ba89c6cac84f5318e1122e326d57e981e56ea7
Maya Saxena <mayasaxena@Mayas-MacBook-Pro.local>
2015-06-09 13:58:07 -0400

    Created SAArtistViewController

---
+++ spotify-artist-viewer/SAArtistViewController.h
@@ -0,0 +1,13 @@
+//
+//  SAArtistViewController.h
+//  spotify-artist-viewer
+//
+//  Created by Maya Saxena on 6/9/15.
+//  Copyright (c) 2015 Intrepid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SAArtistViewController : UIViewController
+
+@end
