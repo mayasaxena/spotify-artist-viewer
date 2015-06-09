c0fd0dc9459b8ea620d54db36b9efe6c3a585e9b
Maya Saxena <mayasaxena@Mayas-MacBook-Pro.local>
2015-06-09 13:52:59 -0400

    Added SASearchViewController

--- spotify-artist-viewer/SASearchViewController.h
+++ spotify-artist-viewer/SASearchViewController.h
@@ -1,14 +1,13 @@
 //
-//  ViewController.h
+//  SASearchViewController.h
 //  spotify-artist-viewer
 //
+//  Created by Maya Saxena on 6/9/15.
 //  Copyright (c) 2015 Intrepid. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface SASearchViewController : UIViewController
 
 @end
-
